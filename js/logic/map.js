0cf01439000d42b19a159c678365066c3e2d6f30
Tobias Rojahn <tobias.rojahn@innogames.de>
2014-03-28 23:30:17 +0100

    gamejam

--- js/logic/map.js
+++ js/logic/map.js
@@ -1,21 +1,37 @@
-function map( grid ) {
+function Map( grid ) {
 	this.rooms = [];
 	this.grid = grid;
 
-	this.checkCollision = function( room, x, y ) {
+	this.checkCollision = function( roomType, x, y ) {
 		var collisions = [];
-		x -= 1; y -= 1;
 
-		for( var rx = 0; rx < room.shape.length; rx++ )
-			for( var ry = 0; ry < room.shape[0].length; ry++ )
-				if( room.shape[rx][ry] && grid[x+rx][ry+y] )
+		x -= 1; y -= 1;
+		for( var rx = 0; rx < roomType.shape.length; rx++ )
+			for( var ry = 0; ry < roomType.shape[0].length; ry++ )
+				if( roomType.shape[rx][ry] && grid[x+rx][ry+y] )
 					collisions.push({x: x+rx, y: y+ry });
 
 		return collisions.length ? collisions : false;
 	};
 
-	this.placeRoom = function( room, x, y ) {
-		if( !this.checkCollision( room, x, y )) return false;
+	this.placeRoom = function( roomType, x, y ) {
+		if( this.checkCollision( roomType, x, y )) return false;
 
+		var roomInstance = new Room( x, y, roomType );
+		this.rooms.push(roomInstance);
+		x -= 1; y -= 1;
+
+		for( var rx = 0; rx < roomType.shape.length; rx++ )
+			for( var ry = 0; ry < roomType.shape[0].length; ry++ )
+				if( roomType.shape[rx][ry] == 1 )
+					grid[x+rx][ry+y] = roomInstance;
 	};
+
+	this.getGrid = function() {
+		return grid;
+	};
+
+	this.update = function() {
+
+	}
 }
