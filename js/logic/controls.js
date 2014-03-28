29bb4ce21e675db54096c4e2d0d1a3f954671b48
Armag3ddon <Armag3ddon@web.de>
2014-03-28 22:28:11 +0100

    Scrolling added

---
+++ js/logic/controls.js
@@ -0,0 +1,24 @@
+/* Game controls */
+
+function initControls() {
+    // Map scrolling
+    Crafty.addEvent(this, Crafty.stage.elem, "mousedown", function(e) {
+        // Do not use second mouse button
+        if (e.button > 1) return;
+
+        var base = {x: e.clientX, y: e.clientY};
+
+        function scroll(e) {
+            var dx = base.x - e.clientX,
+                dy = base.y - e.clientY;
+            base = {x: e.clientX, y: e.clientY};
+            Crafty.viewport.x -= dx;
+            Crafty.viewport.y -= dy;
+        };
+
+        Crafty.addEvent(this, Crafty.stage.elem, "mousemove", scroll);
+        Crafty.addEvent(this, Crafty.stage.elem, "mouseup", function() {
+            Crafty.removeEvent(this, Crafty.stage.elem, "mousemove", scroll);
+        });
+    });
+}
--- js/logic/game.js
+++ js/logic/game.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 
     initIso(100,100);
     initSprites();
+    initControls();
 
     run();
 })
