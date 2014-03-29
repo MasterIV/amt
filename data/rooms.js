71eb07f69261e29db481b0a6736dc8573924d008
purhur <purhur@purh.pw>
2014-03-29 06:18:14 +0100

    some change

--- data/rooms.js
+++ data/rooms.js
@@ -1,7 +1,26 @@
+/*
+ upkeep: 0,
+ capacity: 0,
+
+ worker: 1,
+ upkeep: 100,
+ speed: 20,
+ price: 500,
+ fee: 50,
+
+
+ entertainment: .8,
+ slow:.8,
+ income: 20,
+
+
+ */
+
 var rooms = [
 	{
-		name: "Arbeitsamt",
-		category: ["work"],
+		name: "Tv",
+		category: ["entertain", "slow"],
+		image:'img/raum_tv.png',
 
 		worker: 1,
 		upkeep: 100,
@@ -9,46 +28,44 @@ var rooms = [
 		price: 500,
 		fee: 50,
 
-
 		shape: [
 			[ 2, 2, 2, 2, 2, 2, 2 ],
 			[ 2, 1, 1, 1, 1, 1, 2 ],
 			[ 2, 1, 1, 1, 1, 1, 2 ],
-			[ 2, 1, 1, 1, 1, 1, 2 ],
-			[ 2, 1, 1, 1, 2, 2, 2 ],
-			[ 2, 1, 1, 1, 2, 0, 0 ],
-			[ 2, 2, 2, 2, 2, 0, 0 ]
+			[ 2, 1, 1, 2, 2, 2, 2 ],
+			[ 2, 2, 2, 0, 0, 0, 0 ]
 		]
 	}, {
-		name: "Snackautomat",
+		name: "Snackbox",
 		category: ["entertain", "slow"],
+		image:'img/raum_snackbox.png',
 
 		entertainment: .8,
 		slow:.8,
 		income: 20,
 
 		shape: [
-			[ 2, 2, 2 ],
-			[ 2, 1, 2 ],
-			[ 2, 2, 2 ]
+			[ 2, 2, 2, 2 ],
+			[ 2, 1, 1, 2 ],
+			[ 2, 1, 1, 2 ],
+			[ 2, 1, 1, 2 ],
+			[ 2, 2, 2, 2 ]
 		]
 	},	{
-		name: "Warteraum",
+		name: "Toilette",
 		category: ["wait"],
-
+		image:'img/raum_restroom.png',
 		upkeep: 0,
 		capacity: 0,
-
 
 		shape: [
 			[ 2, 2, 2, 2, 2 ],
 			[ 2, 1, 1, 1, 2 ],
 			[ 2, 1, 1, 1, 2 ],
-			[ 2, 1, 1, 1, 2 ],
-			[ 2, 1, 1, 1, 2 ],
-			[ 2, 1, 1, 1, 2 ],
-			[ 2, 1, 1, 1, 2 ],
-			[ 2, 2, 2, 2, 2 ],
+			[ 2, 1, 1, 2, 2 ],
+			[ 2, 1, 1, 2, 0 ],
+			[ 2, 1, 1, 2, 0 ],
+			[ 2, 2, 2, 2, 0 ],
 		]
 	},
 ];
