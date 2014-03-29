38b9f2b9e65161391284508bee4727e421b22860
purhur <purhur@purh.pw>
2014-03-29 07:25:02 +0100

    some change

--- js/logic/victim.js
+++ js/logic/victim.js
@@ -4,6 +4,7 @@ function Victim(workRoom) {
 	this.x = null;
 	this.y = null;
 	this.currentPath = null;
+	this.movementSpeed = 1;
 
 	this.enter = function() {
 		this.x = 0; // levelstart
@@ -21,6 +22,25 @@ function Victim(workRoom) {
 
 	this.goTo = function(posX, posY) {
 		this.currentPath = a_star([this.x,this.y], [posX,posY], map.grid, MAPWIDHT, MAPHEIGHT, false); // change MAP DIMENSIONS
+		
+	}
+	
+	this.update = function(delta) {
+		if (this.currentPath.length) {
+			 if (this.currentPath[0].x != this.x) {
+				 this.x = this.x + (this.currentPath[0].x>this.x)?(-1*this.movementSpeed*delta):(this.movementSpeed*delta);
+			 }
+			 if (this.currentPath[0].y != this.y) {
+				 this.y = this.y + (this.currentPath[0].y>this.y)?(-1*this.movementSpeed*delta):(this.movementSpeed*delta);
+			 }
+			if (this.currentPath[0].x == this.x && this.currentPath[0].y == this.y) {
+				this.currentPath.splice(0,1);
+			}
+		}
+	}
+	
+	this.draw = function(ctx) {
+		ctx.drawImage( g[img], 0, 0, 32, 16, dx-16, dy, 32, 16);
 	}
 
 	this.leave = function() {
