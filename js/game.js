aa831f91f4bc7fc783b1d2f4016a90919815f9ba
Tobias Rojahn <tobias.rojahn@innogames.de>
2014-03-29 05:36:29 +0100

    replaced engine

--- js/game.js
+++ js/game.js
@@ -14,7 +14,7 @@ var game = {
 	scene: null,
 	lastUpdate: 0,
 	debug: false,
-	scale: 1,
+	zoom: 1,
 
 	display: null,
 	displayCtx: null,
---
+++ js/gui/background.js
@@ -0,0 +1,21 @@
+function Background( grid, offset ) {
+	this.canvas = document.createElement('canvas');
+	var ctx = this.canvas.getContext('2d');
+
+	var w = grid.length;
+	var h = grid[0].length;
+
+	this.canvas.width = (w+h)*16;
+	this.canvas.height = (w+h)*8 + offset.y;
+
+	for( var x = 0; x < w; x++ )
+		for( var y = 0; y < h; y++ )
+			if( grid[x][y] == 0 ){
+				var dx = offset.x+x*16+y*-16;
+				var dy = offset.y+x*8+y*8;
+
+				if( x == 0 || grid[x-1][y] != 0 ) ctx.drawImage(g['img/wall_y.png'], 0, 0, 16, 39, dx-16, dy-31, 16, 39);
+				if( y == 0 || grid[x][y-1] != 0  ) ctx.drawImage(g['img/wall_x.png'], 0, 0, 16, 39, dx, dy-31, 16, 39);
+				ctx.drawImage(g['img/floor.png'], 0, 0, 32, 16, dx-16, dy, 32, 16);
+			}
+}
